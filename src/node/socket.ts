9c42096dbd6aba3ff403e5cbd09e1bdcc1f74777
LAlex <lalex@lalex.com>
2022-02-10 13:53:50 +0000

    Local prettier for dev

--- src/node/socket.ts
+++ src/node/socket.ts
@@ -1,8 +1,8 @@
 import { LG } from "big-l";
 import {
-  Auth,
-  ConnectionOptions,
-  HaWebSocket,
+    Auth,
+    ConnectionOptions,
+    HaWebSocket,
 } from "home-assistant-js-websocket";
 import WebSocket from "ws";
 
@@ -15,142 +15,148 @@ const ERR_CANNOT_CONNECT = 1;
 const ERR_INVALID_AUTH = 2;
 
 const authMessage = (token: string) =>
-  JSON.stringify({ type: "auth", access_token: token });
+    JSON.stringify({ type: "auth", access_token: token });
 
 export type NodeSocketOptions = {
-  retry: number;
-  debug: boolean;
+    retry: number;
+    debug: boolean;
 };
 
 const defaultNodeSocketOptions: NodeSocketOptions = {
-  retry: 3,
-  debug: false,
+    retry: 3,
+    debug: false,
 };
 
 export const createNodeSocket =
-  (socketOptions: Partial<NodeSocketOptions> = {}) =>
-  async (options: ConnectionOptions) => {
-    const auth = options.auth!;
-    const url = auth.wsUrl;
+    (socketOptions: Partial<NodeSocketOptions> = {}) =>
+    async (options: ConnectionOptions) => {
+        const auth = options.auth!;
+        const url = auth.wsUrl;
 
-    const { debug, retry } = { ...defaultNodeSocketOptions, ...socketOptions };
+        const { debug, retry } = {
+            ...defaultNodeSocketOptions,
+            ...socketOptions,
+        };
 
-    let isAuthInvalid = false;
+        let isAuthInvalid = false;
 
-    if (debug) LGR.debug("[AUTH] Init", url);
+        if (debug) LGR.debug("[AUTH] Init", url);
 
-    const connect = (
-      tries: number,
-      promiseResolve: (socket: HaWebSocket) => void,
-      promiseReject: (errorCode: number) => void
-    ) => {
-      const ws = new WebSocket(url, { rejectUnauthorized: false });
-      let haVersion: string;
+        const connect = (
+            tries: number,
+            promiseResolve: (socket: HaWebSocket) => void,
+            promiseReject: (errorCode: number) => void
+        ) => {
+            const ws = new WebSocket(url, { rejectUnauthorized: false });
+            let haVersion: string;
 
-      const handleOpen = async (event: WebSocket.Event) => {
-        try {
-          if (auth.expired) {
-            await auth.refreshAccessToken();
-          }
-          ws.send(authMessage(auth.accessToken));
-        } catch (e) {
-          isAuthInvalid = e === ERR_INVALID_AUTH;
-          ws.close();
-        }
-      };
+            const handleOpen = async (event: WebSocket.Event) => {
+                try {
+                    if (auth.expired) {
+                        await auth.refreshAccessToken();
+                    }
+                    ws.send(authMessage(auth.accessToken));
+                } catch (e) {
+                    isAuthInvalid = e === ERR_INVALID_AUTH;
+                    ws.close();
+                }
+            };
 
-      const handleMessage = async ({
-        data,
-        type,
-        target,
-      }: WebSocket.MessageEvent) => {
-        const message: { type: string; ha_version: string } = JSON.parse(
-          data.toString()
-        );
-        haVersion ??= message.ha_version;
+            const handleMessage = async ({
+                data,
+                type,
+                target,
+            }: WebSocket.MessageEvent) => {
+                const message: { type: string; ha_version: string } =
+                    JSON.parse(data.toString());
+                haVersion ??= message.ha_version;
 
-        switch (message.type) {
-          case MSG_TYPE_AUTH_INVALID:
-            isAuthInvalid = true;
-            ws.close();
+                switch (message.type) {
+                    case MSG_TYPE_AUTH_INVALID:
+                        isAuthInvalid = true;
+                        ws.close();
 
-          case MSG_TYPE_AUTH_OK:
-            if (debug)
-              LGR.verb(
-                "[AUTH] WebSocket connection to Home Assistant authenticated"
-              );
-            ws.removeEventListener("open", handleOpen);
-            ws.removeEventListener("message", handleMessage);
-            ws.removeEventListener("close", handleClose);
-            ws.removeEventListener("error", handleError);
-            const haws = ws as unknown as HaWebSocket;
-            haws.haVersion = haVersion;
-            promiseResolve(haws);
-        }
-      };
+                    case MSG_TYPE_AUTH_OK:
+                        if (debug)
+                            LGR.verb(
+                                "[AUTH] WebSocket connection to Home Assistant authenticated"
+                            );
+                        ws.removeEventListener("open", handleOpen);
+                        ws.removeEventListener("message", handleMessage);
+                        ws.removeEventListener("close", handleClose);
+                        ws.removeEventListener("error", handleError);
+                        const haws = ws as unknown as HaWebSocket;
+                        haws.haVersion = haVersion;
+                        promiseResolve(haws);
+                }
+            };
 
-      const handleClose = async ({
-        wasClean,
-        code,
-        reason,
-        target,
-      }: WebSocket.CloseEvent) => {
-        let errorMessage: string | undefined;
-        if (code && code !== 1000) {
-          errorMessage = `WebSocket connection to Home Assistant closed with code ${code} and reason ${reason}`;
-        }
-        close(errorMessage);
-      };
+            const handleClose = async ({
+                wasClean,
+                code,
+                reason,
+                target,
+            }: WebSocket.CloseEvent) => {
+                let errorMessage: string | undefined;
+                if (code && code !== 1000) {
+                    errorMessage = `WebSocket connection to Home Assistant closed with code ${code} and reason ${reason}`;
+                }
+                close(errorMessage);
+            };
 
-      const handleError = async ({
-        error,
-        message,
-        type,
-        target,
-      }: WebSocket.ErrorEvent) => {
-        ws.removeEventListener("close", handleClose);
-        let errorMessage =
-          "Disconnected from Home Assistant with a WebSocket error";
-        if (message) errorMessage += ` with message: ${message}`;
-        close(errorMessage);
-      };
+            const handleError = async ({
+                error,
+                message,
+                type,
+                target,
+            }: WebSocket.ErrorEvent) => {
+                ws.removeEventListener("close", handleClose);
+                let errorMessage =
+                    "Disconnected from Home Assistant with a WebSocket error";
+                if (message) errorMessage += ` with message: ${message}`;
+                close(errorMessage);
+            };
 
-      const close = (errorMessage?: string) => {
-        if (errorMessage) {
-          LGR.error(
-            `WebSocket Connection to HA close with message ${errorMessage}`
-          );
-        }
-        if (isAuthInvalid) {
-          promiseReject(ERR_INVALID_AUTH);
-          return;
-        }
-        if (tries === 0) {
-          promiseReject(ERR_CANNOT_CONNECT);
-          return;
-        }
+            const close = (errorMessage?: string) => {
+                if (errorMessage) {
+                    LGR.error(
+                        `WebSocket Connection to HA close with message ${errorMessage}`
+                    );
+                }
+                if (isAuthInvalid) {
+                    promiseReject(ERR_INVALID_AUTH);
+                    return;
+                }
+                if (tries === 0) {
+                    promiseReject(ERR_CANNOT_CONNECT);
+                    return;
+                }
 
-        setTimeout(
-          () =>
-            connect(tries < -1 ? -1 : tries - 1, promiseResolve, promiseReject),
-          1000
-        );
-      };
+                setTimeout(
+                    () =>
+                        connect(
+                            tries < -1 ? -1 : tries - 1,
+                            promiseResolve,
+                            promiseReject
+                        ),
+                    1000
+                );
+            };
 
-      const messageDebug = async ({
-        data,
-        type,
-        target,
-      }: WebSocket.MessageEvent) => {
-        LGR.debug("[DEBUG]", type, JSON.parse(data.toString()));
-      };
+            const messageDebug = async ({
+                data,
+                type,
+                target,
+            }: WebSocket.MessageEvent) => {
+                LGR.debug("[DEBUG]", type, JSON.parse(data.toString()));
+            };
 
-      ws.addEventListener("open", handleOpen);
-      if (debug) ws.addEventListener("message", messageDebug);
-      ws.addEventListener("message", handleMessage);
-      ws.addEventListener("close", handleClose);
-      ws.addEventListener("error", handleError);
+            ws.addEventListener("open", handleOpen);
+            if (debug) ws.addEventListener("message", messageDebug);
+            ws.addEventListener("message", handleMessage);
+            ws.addEventListener("close", handleClose);
+            ws.addEventListener("error", handleError);
+        };
+
+        return new Promise<HaWebSocket>((res, rej) => connect(retry, res, rej));
     };
-
-    return new Promise<HaWebSocket>((res, rej) => connect(retry, res, rej));
-  };
--- src/test.ts
+++ src/test.ts
@@ -9,53 +9,53 @@ const scriptName = "test-ha-js-sugar";
 const LGR = LG.ns(scriptName);
 
 const ACCESS_TOKEN =
-  "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiI3MjI1MDM4NDZjNzI0MzFmODYwNmI0ZTViNWYxMTVjNyIsImlhdCI6MTY0NDQyNTk5NywiZXhwIjoxOTU5Nzg1OTk3fQ.QBliLICX80o98AUADQBQU2I2mzhjxX1XkDDytabxkPk";
+    "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiI5NDdhMmIyZDczMWM0YTQ5YTE3ZTM3NjRiODlhNDM5NiIsImlhdCI6MTY0NDUwMDg5NSwiZXhwIjoxOTU5ODYwODk1fQ.nspN8kA_sSHKVpikmxlFa6LHeONY2y-sh6ANBc3e4QE";
 
 (async function () {
-  try {
-    for (const evt of [
-      "beforeExit",
-      "uncaughtException",
-      "SIGTSTP",
-      "SIGQUIT",
-      "SIGHUP",
-      "SIBABRT",
-      "SIGTERM",
-      "SIGINT",
-    ]) {
-      process.on(evt, async () => {
-        LGR.crit(`!! ${scriptName} interrupted by ${evt} !!`);
+    try {
+        for (const evt of [
+            "beforeExit",
+            "uncaughtException",
+            "SIGTSTP",
+            "SIGQUIT",
+            "SIGHUP",
+            "SIBABRT",
+            "SIGTERM",
+            "SIGINT",
+        ]) {
+            process.on(evt, async () => {
+                LGR.crit(`!! ${scriptName} interrupted by ${evt} !!`);
+                process.exit(1);
+            });
+        }
+
+        LGR.notice(`:: START ${scriptName} ::`);
+
+        const conn = await Connection.connect(
+            "http://127.0.0.1:8123",
+            ACCESS_TOKEN
+        );
+
+        const lgt = conn.getEntity<Light>("light.virtual_light_1");
+        const sw = conn.getEntity<Switch>("switch.virtual_switch_1");
+        const hacs = conn.getEntity<Sensor>("sensor.hacs");
+        const fan = conn.getEntity<Fan>("fan.virtual_office_fan");
+        const tog = conn.getEntity<Switch>("input_boolean.toggle1");
+        const tog2 = conn.getEntity<Switch>("input_boolean.toggle2");
+        if (sw) LGR.debug(sw.domain, "=>", sw.id);
+        LGR.debug("HACS:", hacs?.toHumanString());
+        tog?.addStateListener(
+            async (lgt: Switch, oldState) => {
+                await tog2?.setState(lgt.state);
+                await fan?.setState(lgt.state);
+            },
+            { oldState: true }
+        );
+
+        LGR.notice(`-- ${scriptName} DONE --`);
+        // process.exit();
+    } catch (e) {
+        LGR.crit(`!! ${scriptName} ERROR ${(e as Error)?.message || e} !!`);
         process.exit(1);
-      });
     }
-
-    LGR.notice(`:: START ${scriptName} ::`);
-
-    const conn = await Connection.connect(
-      "http://127.0.0.1:8123",
-      ACCESS_TOKEN
-    );
-
-    const lgt = conn.getEntity<Light>("light.virtual_light_1");
-    const sw = conn.getEntity<Switch>("switch.virtual_switch_1");
-    const hacs = conn.getEntity<Sensor>("sensor.hacs");
-    const fan = conn.getEntity<Fan>("fan.virtual_office_fan");
-    const tog = conn.getEntity<Switch>("input_boolean.toggle");
-    const tog2 = conn.getEntity<Switch>("input_boolean.toggle2");
-    if (sw) LGR.debug(sw.domain, "=>", sw.id);
-    LGR.debug("HACS:", hacs?.toHumanString());
-    tog?.addStateListener(
-      async (lgt: Switch, oldState) => {
-        await tog2?.setState(lgt.state);
-        await fan?.setState(lgt.state);
-      },
-      { oldState: true }
-    );
-
-    LGR.notice(`-- ${scriptName} DONE --`);
-    // process.exit();
-  } catch (e) {
-    LGR.crit(`!! ${scriptName} ERROR ${(e as Error)?.message || e} !!`);
-    process.exit(1);
-  }
 })();
